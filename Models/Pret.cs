a0fa758ab4fadf3364bb61dd8dbc3c8c82efd9b0
Fahmi Derbali <fderbali@users.noreply.github.com>
2022-04-30 02:37:59 -0400

    Du wording et du styling

--- Models/Pret.cs
+++ Models/Pret.cs
@@ -9,18 +9,19 @@ namespace BibliothequeVieuxMontreal.Models
     {
         public int Id { get; set; }
         [Required]
-        [DisplayName("Numéro du livre")]
+        [DisplayName("Livre")]
         public int IdLivre { get; set; }
-        [DisplayName("Numéro du membre")]
         [Required]
+        [DisplayName("Membre")]
         public int IdMembre { get; set; }
         [Required]
         [DisplayName("Date de début")]
         public DateTime DateDebut { get; set; }
         [DisplayName("Date de Fin")]
         public DateTime DateFin { get; set; }
-
+        [DisplayName("Livre")]
         public virtual Livre IdLivreNavigation { get; set; } = null!;
+        [DisplayName("Membre")]
         public virtual Membre IdMembreNavigation { get; set; } = null!;
     }
 }
