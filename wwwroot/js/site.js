1ed9df1ad063b6a659adcca5744b7ad5ce717e24
Fahmi Derbali <fderbali@users.noreply.github.com>
2022-04-30 02:48:04 -0400

    Installation du plugin DataTable

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+jQuery('document').ready(function () {
+    jQuery('table').DataTable({
+        aoColumnDefs: [
+            { "aTargets": [0], "bSortable": true },
+            { "aTargets": [2], "asSorting": ["asc"], "bSortable": true },
+        ],
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/French.json"
+        }
+    });
+});
